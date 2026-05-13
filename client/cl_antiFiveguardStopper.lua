5e40a029c9d895b1c13b5ef967da4a708b080d75
Jeakels <136957358+jeakels@users.noreply.github.com>
2026-05-13 02:42:13 +0200

    Renamed for easy understanding

---
+++ client/cl_antiFiveguardStopper.lua
@@ -0,0 +1,10 @@
+local data = LoadResourceFile(CurrentResourceName,'config.lua')
+local Config = assert(load(data))()?.AntiFiveguardStopper
+if not Config?.enable then return end
+while not READY do Citizen.Wait(0) end
+
+local function check()
+    TriggerServerEvent('fg:addon:resourceState', GetResourceState(Fiveguard) == 'started')
+    Citizen.SetTimeout(Config.checkInterval * 1000, check)
+end
+check()
---
+++ server/sv_antiFiveguardStopper.lua
@@ -0,0 +1,20 @@
+local data = LoadResourceFile(CurrentResourceName,'config.lua')
+local Config = assert(load(data))()?.AntiFiveguardStopper
+if not Config?.enable then return end
+while not READY do Citizen.Wait(0) end
+local playerStates = {}
+
+local function check()
+    for playerId, state in pairs(playerStates) do
+        if not state then
+            PunishPlayer(playerId, Config.ban, 'Stopped Fiveguard', false)
+            playerStates[playerId] = nil
+        end
+    end
+    Citizen.SetTimeout(Config.checkInterval * 1000, check)
+end
+check()
+
+RegisterNetEvent('fg:addon:resourceState', function(isResourceActive)
+    playerStates[source] = isResourceActive
+end)
